aada6b109afa524bc10d128b6d1aa55badaf731e
Murillo Lannes <118365053+Mulannes@users.noreply.github.com>
2023-02-14 16:38:56 -0300

    Upload

---
+++ Programação/Início3.Javascript/script.js
@@ -0,0 +1,21 @@
+//function testingvariables(){
+//    const nomedavariavel = 'Olá Murillo'
+//    console.log(nomedavariavel);
+//
+//    }
+//    function testGlobal(){
+//        const nomedavariavel = 'São Paulo'
+//        console.log(nomedavariavel);
+//
+//}
+//testingvariables();
+//testGlobal(); 
+//function imprimenome(nome){
+//    console.log(nome);
+//}
+
+//imprimenome("murillo");
+
+const imprimenome = (nome) => console.log(nome);
+
+imprimenome('Murillo');
---
+++ Programação/TypeScript.ts/adadsa.js
@@ -0,0 +1,10 @@
+console.log('=============Beatiful=============')
+
+var nome = "Murillo"
+
+console.log(nome)
+
+function soma(numero){
+    console.log(numero + 15)
+}
+soma(10);
